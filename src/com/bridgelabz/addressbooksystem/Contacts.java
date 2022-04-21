25d7fa74f536f5198158df41c6f2fafd380ca5f1
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-21 15:56:29 +0530

    [ADDED] Created Contact, AddressBook Class with Variables

---
+++ src/com/bridgelabz/addressbooksystem/Contacts.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbooksystem;
+/*
+ * @author : Ashwini
+ * Refactor to adAbility to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...
+*/
+public class Contacts {
+//Variable Declaration for Contacts Class
+    String firstName;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNo;
+    String emailID;
+} 
