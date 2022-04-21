bccd87b108b4c6d38d07f681485b48ae2a28a8ae
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-21 15:58:10 +0530

    [ADDED] : Project Initialize for Address Book Problem

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbooksystem;
+/*
+ * @author : Ashwini
+ * 
+ */
+public class AddressBookMain {
+	public static void main(String[] args) {
+        //Welcome message for Users
+        System.out.println("Welcome to Address Book System Problem.");
+    }
+}
